a4586e9f2fa41ebdcdbe8f179bec283e84bf60d2
_Azure <f2v74yt@gmail.com>
2022-08-17 20:36:07 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -9,6 +9,10 @@ wyvern.events = {}
 wyvern.DefaultFolderPath = "Wyvern-Source/"
 wyvern.prefix = "w."
 wyvern.logName = "Wyvern-Log"
+local fileExtension = "wyrn"
+if syn and DrawingImmediate then fileExtension = "txt" end
+local fileExLen = #fileExtension + 1
+
 
  wyvern.config = {
     ['Modules'] = "/Modules",
@@ -37,6 +41,50 @@ wyvern.modules = {
     ["Other"]  =  "Other"
 }
 
+wyvern.util.randomStringP = function()
+	local min, max, final = ("A"):byte(), ("Z"):byte(), "IYPluginDownloader-"
+	for i = 1, math.random(5, 10) do
+		final = final .. string.char(math.random(min, max))
+	end
+	return final
+end
+
+wyvern.util.requestGet = function(url)
+  local rhttp = game:GetService('HttpService') 
+  local req = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request
+if req then
+  local response = req({
+    Url = url,
+    Method = 'GET',
+  })
+  if not response then 
+    wyvern.util.log("Error: Request failed to get " .. url) 
+  end
+  return response.Body
+else
+  wyvern.util.makeLog("Exploit does not support request. This plugin will not work.", "HttpService not found")
+end
+
+wyvern.util.getFileName(attemptName, RAW)
+local fileName
+if attemptName then
+    if attemptName:sub(-fileExLen) == '.'  then
+  fileName = attemptName
+else
+  fileName = attemptName ..'.' 
+    end
+    if not isfile(fileName) then
+        return fileName
+    else
+      wyvern.util.makeLog("Provided file name already exists.", "Error")
+    end
+else
+    fileName = loadstring(RAW)().PluginName .. "." 
+    if not isfile(fileName) then return fileName end
+    repeat fileName = randomStringP() .. "."  until not isfile(fileName)
+    return fileName
+end
+end
 
 wyvern.util.get_module = function(module)
     return wyvern.modules[module]
@@ -52,27 +100,45 @@ wyvern.util.get_plugin = function(plugin)
 end
 
 wyvern.util.download_plugin = function(plugin)
-    local url = "https://raw.githubusercontent.com/Wyvern-Admin/Wyvern-Admin/master/Wyvern-Admin/Plugins/" .. plugin .. ".lua"
+    local url = "https://raw.githubusercontent.com/Zirmith/Wyvern-Admin/main/Plugins/" .. plugin .. ".wyrn"
+    local pluginRaw = wyvern.util.requestGet(url)
+    fileName = loadstring(pluginRaw)().PluginName .. ".lua" 
+	writefile('Wyvern-Source/Plugins/'..fileName, pluginRaw)
 end
 
 wyvern.util.download_hook = function(hook)
-    local url = "https://raw.githubusercontent.com/Wyvern-Admin/Wyvern-Admin/master/Wyvern-Admin/Hooks/" .. hook .. ".lua"
+    local url = "https://raw.githubusercontent.com/Zirmith/Wyvern-Admin/main/Hooks/" .. hook .. ".wyrn"
+    local pluginRaw = wyvern.util.requestGet(url)
+    fileName = loadstring(pluginRaw)().HookName ..  ".lua" 
+	writefile('Wyvern-Source/Hooks/'..fileName, pluginRaw)
 end
 
 wyvern.util.download_event = function(event)
-    local url = "https://raw.githubusercontent.com/Wyvern-Admin/Wyvern-Admin/master/Wyvern-Admin/Events/" .. event .. ".lua"
+    local url = "https://raw.githubusercontent.com/Zirmith/Wyvern-Admin/main/Events/" .. event .. ".wyrn"
+    local pluginRaw = wyvern.util.requestGet(url)
+    fileName = loadstring(pluginRaw)().EventName ..  ".lua" 
+	writefile('Wyvern-Source/Events/'..fileName, pluginRaw)
 end
 
 wyvern.util.download_command = function(command)
-    local url = "https://raw.githubusercontent.com/Wyvern-Admin/Wyvern-Admin/master/Wyvern-Admin/Commands/" .. command .. ".lua"
+    local url = "https://raw.githubusercontent.com/Zirmith/Wyvern-Admin/main/Commands/" .. command .. ".wyrn"
+    local pluginRaw = wyvern.util.requestGet(url)
+    fileName = loadstring(pluginRaw)().CommandName .. ".lua" 
+	writefile('Wyvern-Source/Commands/'..fileName, pluginRaw)
 end
 
 wyvern.util.download_config = function(config)
-    local url = "https://raw.githubusercontent.com/Wyvern-Admin/Wyvern-Admin/master/Wyvern-Admin/Config/" .. config .. ".lua"
+    local url = "https://raw.githubusercontent.com/Zirmith/Wyvern-Admin/main/Config/" .. config .. ".wyrn"
+    local pluginRaw = wyvern.util.requestGet(url)
+    fileName = loadstring(pluginRaw)().ConfigName .. ".lua"  
+	writefile('Wyvern-Source/Configs/'..fileName, pluginRaw)
 end
 
 wyvern.util.download_module = function(module)
-    local url = "https://raw.githubusercontent.com/Wyvern-Admin/Wyvern-Admin/master/Wyvern-Admin/Modules/" .. module .. ".lua"
+    local url = "https://raw.githubusercontent.com/Zirmith/Wyvern-Admin/main/Modules/" .. module .. ".wyrn"
+    local pluginRaw = wyvern.util.requestGet(url)
+    fileName = loadstring(pluginRaw)().ModuleName ..  ".lua"  
+	writefile('Wyvern-Source/Modules/'..fileName, pluginRaw)
 end
 
 wyvern.util.checkIfPluginExists = function(plugin)
@@ -137,5 +203,4 @@ for i,v in pairs(wyvern.config) do
 end
 
 
--- make log 
-wyvern.util.makeLog("Wyvern Admin Started", "info")
+wyvern.util.download_plugin('example')
